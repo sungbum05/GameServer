9ad332b6ce87c0ee1829d918fb8a01cec1e44476
sungbum05 <sblim05@naver.com>
2023-07-10 02:41:28 +0900

    FrameFix

--- Assets/Script/Constants.cs
+++ Assets/Script/Constants.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class Constants
 {
-    public const int TICKS_PER_SEC = 100;
+    public const int TICKS_PER_SEC = 120;
     public const int MS_PER_TICK = 1000 / TICKS_PER_SEC;
 }
